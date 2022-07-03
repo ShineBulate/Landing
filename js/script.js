ad2573ff98854eacde574ecab7c8182f6db1b78a
Basheev Bulat <akikanee@MacBook-Air-Basheev.local>
2022-07-03 23:12:31 +0800

    beta v 0.7

--- js/script.js
+++ js/script.js
@@ -1,9 +1,23 @@
-var app = new Vue({
-    el:'#vue',
-    data: { 
 
-        message: 'Welcome to Vue'
-    }
+window.onload = function(){
+   
+    let btn = document.querySelectorAll('button');
+    btn.forEach(function(element) {
+        element.onclick = showModal;
+    });
+    
+    let modal_view = document.getElementById('modal_view');
+    let span = document.getElementsByClassName("close")[0];
+       
+    function showModal(){
+          
+         modal_view.style.display = "block";
+}
+    span.onclick = function() {
+        modal_view.style.display = "none";
+      }
+    
 
-});
-app.message = 'I changed this message ;)';
+
+  
+}
