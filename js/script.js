6694056e23c4ecf7577661816826face7d8cba3e
Basheev Bulat <akikanee@MacBook-Air-Basheev.local>
2022-07-07 10:11:13 +0800

    beta v 1.0 added vue js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,35 @@
+new Vue({
+    el: '#user-column',
+    data:{
+        text:[
+            {id:1, head:'Почему углеродный след Utopia P2P низкий?', message:'Все данные пользователей шифруются и передаются по ячеистой сети. В результате не используются дополнительные источники питания.',},
+            {id:2, head:'Как Utopia P2P экономит электроэнергию?',message:'Какой то текст'},
+            {id:3, head:'Безопасна ли Утопия?',message:'Ваш текст',}
+        ],
+        isActive: 0,
+    },
 
+    methods:{
+        open:function(){
+        
+            switch(this.isActive){
+                case 0: this.isActive = 1;
+                break;
+                case 1: this.isActive = 0;
+                break;
+            }
+         
+            
+            
+        },
+    }
+})
 window.onload = function(){
-   
     let btn = document.querySelectorAll('button');
     btn.forEach(function(element) {
         element.onclick = showModal;
-    });
-    
+    })
+
     let modal_view = document.getElementById('modal_view');
     let span = document.getElementsByClassName("close")[0];
        
@@ -16,8 +40,6 @@ window.onload = function(){
     span.onclick = function() {
         modal_view.style.display = "none";
       }
-    
-
-
-  
+ 
 }
+        
